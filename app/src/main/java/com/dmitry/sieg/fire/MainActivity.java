0ae5f99f13ed1f49ab3b9a9a217c1a2b9a8339e4
d.boldyrev <dmitry.sieg@gmail.com>
2018-05-30 12:40:00 +0300

    Made working under 6.0

--- app/src/main/java/com/dmitry/sieg/fire/MainActivity.java
+++ app/src/main/java/com/dmitry/sieg/fire/MainActivity.java
@@ -1,20 +1,13 @@
 package com.dmitry.sieg.fire;
 
 import android.annotation.SuppressLint;
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
 
 public class MainActivity extends Activity {
 
@@ -25,11 +18,8 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
-        final ImageView imageView = findViewById(R.id.imageview);
-        Glide.with(this)
-                .load(R.drawable.fire)
-                .into(imageView);
-        imageView.setOnTouchListener(new FireTouchListener(this));
+        final View view = findViewById(R.id.gifview);
+        view.setOnTouchListener(new FireTouchListener(this));
     }
 
     @Override
